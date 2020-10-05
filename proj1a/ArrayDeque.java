2cf28d30d21d939921b262b2597e85095c04cb37
YueTeng <a370016227@hotmail.com>
2020-10-05 22:44:25 +0800

    Better Style.

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -5,122 +5,126 @@ public class ArrayDeque<T> {
     private int first;
     private int last;
 
-    public ArrayDeque(){
+    public ArrayDeque() {
         items = (T []) new Object[8];
         size = 0;
     }
 
-    public void addFirst(T item){
-        if(size == 0){
+    public void addFirst(T item) {
+        if (size == 0) {
             items[0] = item;
             first = 0;
-        }else{
-            if(size!=items.length){
-                if(first == 0){
+            last = 0;
+        } else {
+            if (size != items.length) {
+                if (first == 0) {
                     first = items.length - 1;
-                }else{
+                } else {
                     first -= 1;
                 }
-                items[first] = item;
-            } else{
-                resize(size*refactor);
-                addFirst(item);
+
+            } else {
+                resize(size * refactor);
+                first = items.length - 1;
             }
+            items[first] = item;
         }
 
         size += 1;
     }
 
-    public void addLast(T item){
-        if(size == 0){
+    public void addLast(T item) {
+        if (size == 0) {
             items[0] = item;
             last = 0;
-        }else{
-            if(size!=items.length){
-                if(last == items.length-1){
+            first = 0;
+        } else {
+            if(size != items.length) {
+                if(last == items.length-1) {
                     last = 0;
-                }else{
+                } else {
                     last = size+first-items.length;
                     last = convert(last);
                 }
-                items[last] = item;
-            } else{
-                resize(size*refactor);
-                addLast(item);
+            } else {
+                resize(size * refactor);
+                last += 1;
             }
+            items[last] = item;
         }
 
         size += 1;
     }
 
-    public T removeFirst(){
-        if(this.isEmpty()){
+    public T removeFirst() {
+        if (this.isEmpty()) {
             return null;
         }
         T temp = items[first];
         items[first] = null;
-        if(first==items.length-1){
+        if(first == items.length-1) {
             first = 0;
-        }else {
+        } else {
             first += 1;
         }
         size -= 1;
-        if(getUsageRatio()<0.25){
-            resize((int)0.5*items.length);
+        if(items.length >= 16 && getUsageRatio()<0.25) {
+            resize((int)(0.5 * items.length));
         }
         return temp;
     }
 
-    public T removeLast(){
-        if(this.isEmpty()){
+    public T removeLast() {
+        if (this.isEmpty()) {
             return null;
         }
+
         T temp = items[last];
         items[last] = null;
-        if(last==0){
+        if (last==0) {
             last = items.length-1;
-        }else {
+        } else {
             last -= 1;
         }
         size -= 1;
-        if(getUsageRatio()<0.25){
-            resize((int)0.5*items.length);
+        if (items.length >= 16 && getUsageRatio()<0.25) {
+            resize((int) (0.5 * items.length));
         }
         return temp;
     }
 
-    public boolean isEmpty(){
+    public boolean isEmpty() {
         return size == 0 ;
     }
 
-    public int size(){
+    public int size() {
         return size;
     }
 
-    public void printDeque(){
-        if(first>last){
-            for(int i=first;i<=last;i++){
+    public void printDeque() {
+        if(first <= last) {
+            for(int i = first; i <= last; i++) {
                 System.out.print(items[i]);
                 System.out.print(" ");
             }
         }else{
-            for(int j=first;j<items.length;j++) {
+            for(int j = first; j < items.length; j++) {
                 System.out.print(items[j]);
                 System.out.print(" ");
             }
-            for(int j=0;j<=last;j++){
+            for(int j = 0; j <= last; j++) {
                 System.out.print(items[j]);
                 System.out.print(" ");
             }
         }
     }
 
-    private void resize(int cap){
+    private void resize(int cap) {
         T[] a = (T []) new Object[cap];
-        if(first>last){
+        if(first > last) {
             System.arraycopy(items,first,a,0,items.length-first);
             System.arraycopy(items,0,a,items.length-first,last+1);
-        }else{
+        } else {
             System.arraycopy(items,first,a,0,size);
         }
         items = a;
@@ -129,26 +133,26 @@ public class ArrayDeque<T> {
     }
 
     /* when first/last pointer becomes negative, convert the index to the corresponding positive */
-    private int convert(int i){
-        if(i>=0){
+    private int convert(int i) {
+        if (i >= 0) {
             return i;
-        }else{
+        } else {
             return items.length+i;
         }
     }
-    public T get(int i){
-        if(first<=last){
+    public T get(int i) {
+        if (first <= last) {
             return items[first+i];
-        }else{
-            if(first+i<items.length){
+        } else {
+            if (first + i < items.length) {
                 return items[first+i];
-            }else{
+            } else {
                 return items[i+first-items.length];
             }
         }
 
     }
-    private float getUsageRatio(){
-        return size/items.length;
+    private float getUsageRatio() {
+        return (float) size/items.length;
     }
 }
