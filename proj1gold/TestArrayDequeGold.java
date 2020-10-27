e0aa7f131baf1c82fdd7e104d475a7bf926a7ae9
YueTeng <a370016227@hotmail.com>
2020-10-28 00:17:36 +0800

    double the iteration times and enlarge the chances of testadd

--- proj1gold/TestArrayDequeGold.java
+++ proj1gold/TestArrayDequeGold.java
@@ -26,24 +26,29 @@ public class TestArrayDequeGold {
         int size = 0;
         String info = "";
 
-        for (int i = 0; i < 100; i +=1) {
+        for (int i = 0; i < 200; i +=1) {
             double r = StdRandom.uniform();
             Integer a = (int)StdRandom.uniform(1,11);
             if (size == 0) {
-                testAddLast(a);
-                info += "addLast("+a.toString()+")\n";
+                if (StdRandom.uniform() > 0.5) {
+                    testAddLast(a);
+                    info += "addLast("+a.toString()+")\n";
+                } else {
+                    testAddFirst(a);
+                    info += "addFirst("+a.toString()+")\n";
+                }
                 size += 1;
                 continue;
             }
-            if (r < 0.25) {
+            if (r < 0.3) {
                 testAddFirst(a);
                 info += "addFirst("+a.toString()+")\n";
                 size += 1;
-            } else if(r < 0.5) {
+            } else if(r < 0.6) {
                 testAddLast(a);
                 info += "addLast("+a.toString()+")\n";
                 size += 1;
-            } else if(r < 0.75) {
+            } else if(r < 0.8) {
                 testRemoveFirst(info);
                 info += "removeFirst()\n";
                 size -= 1;
