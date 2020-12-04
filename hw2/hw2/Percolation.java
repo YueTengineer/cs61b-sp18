46032aa4a4ca7928d656b6a0e58fa94247ae2d6b
YueTeng <a370016227@hotmail.com>
2020-12-04 22:06:02 +0800

    first commit.

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -3,5 +3,140 @@ package hw2;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
+    private WeightedQuickUnionUF pcl;
+    private WeightedQuickUnionUF pcl2;
+    // false:blocked true:open
+    private boolean [][] state;
+    private int open_num = 0;
+    int length;
+    public Percolation(int N) {
+        if (N <= 0) {
+            throw new IllegalArgumentException("N should be greater than zero.");
+        }
+        /* pcl including the virtual top site and bottom site.
+        TOP SITE : N*N  BOTTOM SITE: N*N + 1.
+        pcl2 only including the virtual top site N*N*/
+        pcl = new WeightedQuickUnionUF(N * N + 2);
+        pcl2 = new WeightedQuickUnionUF(N * N + 1);
+        state = new boolean[N][N];
+        length = N;
+        // connect top layer to the virtual top site and bottom layer to the virtual bottom site.
+        for (int i = 0; i < N; i += 1) {
+            pcl.union(N * N,i);
+            pcl.union(N * N + 1,N * N - i);
+            pcl2.union(N * N,i);
+        }
 
+    }
+    public void open(int row, int col) {
+        if (state[row][col] == true) {
+            return;
+        } else {
+            state[row][col] = true;
+            open_num += 1;
+            if (row == 0) {
+                if (col == 0) {
+                    if (isOpen(row + 1, col)) {
+                        pcl.union(dChange(row, col),dChange(row + 1, col));
+                        pcl2.union(dChange(row, col),dChange(row + 1, col));
+                    }
+                    if (isOpen(row , col + 1)) {
+                        pcl.union(dChange(row, col),dChange(row , col + 1));
+                        pcl2.union(dChange(row, col),dChange(row , col + 1));
+                    }
+                } else if (col == length - 1) {
+                    if (isOpen(row + 1, col)) {
+                        pcl.union(dChange(row, col),dChange(row + 1, col));
+                        pcl2.union(dChange(row, col),dChange(row + 1, col));
+                    }
+                    if (isOpen(row , col - 1)) {
+                        pcl.union(dChange(row, col),dChange(row , col - 1));
+                        pcl2.union(dChange(row, col),dChange(row , col - 1));
+                    }
+                } else {
+                    if (isOpen(row + 1, col)) {
+                        pcl.union(dChange(row, col),dChange(row + 1, col));
+                        pcl2.union(dChange(row, col),dChange(row + 1, col));
+                    }
+                    if (isOpen(row , col - 1)) {
+                        pcl.union(dChange(row, col),dChange(row , col - 1));
+                        pcl2.union(dChange(row, col),dChange(row , col - 1));
+                    }
+                    if (isOpen(row , col + 1)) {
+                        pcl.union(dChange(row, col),dChange(row , col + 1));
+                        pcl2.union(dChange(row, col),dChange(row , col + 1));
+                    }
+                }
+            } else if (row == length - 1) {
+                if (col == 0) {
+                    if (isOpen(row - 1, col)) {
+                        pcl.union(dChange(row, col),dChange(row - 1, col));
+                        pcl2.union(dChange(row, col),dChange(row - 1, col));
+                    }
+                    if (isOpen(row , col + 1)) {
+                        pcl.union(dChange(row, col),dChange(row , col + 1));
+                        pcl2.union(dChange(row, col),dChange(row , col + 1));
+                    }
+                } else if (col == length - 1) {
+                    if (isOpen(row - 1, col)) {
+                        pcl.union(dChange(row, col),dChange(row - 1, col));
+                        pcl2.union(dChange(row, col),dChange(row - 1, col));
+                    }
+                    if (isOpen(row , col - 1)) {
+                        pcl.union(dChange(row, col),dChange(row , col - 1));
+                        pcl2.union(dChange(row, col),dChange(row , col - 1));
+                    }
+                } else {
+                    if (isOpen(row - 1, col)) {
+                        pcl.union(dChange(row, col),dChange(row - 1, col));
+                        pcl2.union(dChange(row, col),dChange(row - 1, col));
+                    }
+                    if (isOpen(row , col - 1)) {
+                        pcl.union(dChange(row, col),dChange(row , col - 1));
+                        pcl2.union(dChange(row, col),dChange(row , col - 1));
+                    }
+                    if (isOpen(row , col + 1)) {
+                        pcl.union(dChange(row, col),dChange(row , col + 1));
+                        pcl2.union(dChange(row, col),dChange(row , col + 1));
+                    }
+                }
+            } else {
+                if (isOpen(row + 1, col)) {
+                    pcl.union(dChange(row, col),dChange(row + 1, col));
+                    pcl2.union(dChange(row, col),dChange(row + 1, col));
+                }
+                if (isOpen(row - 1, col)) {
+                    pcl.union(dChange(row, col),dChange(row - 1, col));
+                    pcl2.union(dChange(row, col),dChange(row - 1, col));
+                }
+                if (isOpen(row , col - 1)) {
+                    pcl.union(dChange(row, col),dChange(row , col - 1));
+                    pcl2.union(dChange(row, col),dChange(row , col - 1));
+                }
+                if (isOpen(row , col + 1)) {
+                    pcl.union(dChange(row, col),dChange(row , col + 1));
+                    pcl2.union(dChange(row, col),dChange(row , col + 1));
+                }
+            }
+        }
+    }
+
+    public boolean isOpen(int row, int col) {
+        return state[row][col] == true;
+    }  // is the site (row, col) open?
+    public boolean isFull(int row, int col) {
+        return pcl2.connected(dChange(row,col),length * length);
+    }  // is the site (row, col) full?
+    public int numberOfOpenSites() {
+        return open_num;
+    }          // number of open sites
+    public boolean percolates() {
+        return pcl.connected(length * length,length * length + 1);
+    }              // does the system percolate?
+    private int dChange(int row, int col) { // change 2d to 1d dimension
+        return row * length + col;
+    }
+    public static void main(String[] args) {
+
+    }   // use for unit testing (not required)
 }
--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,5 +1,54 @@
 package hw2;
 
+import edu.princeton.cs.algs4.StdRandom;
+
 public class PercolationStats {
+    double mean;
+    double stddev;
+    double confidencelow;
+    double confidencehigh;
+    int [] openls;
+    public PercolationStats(int N, int T, PercolationFactory pf) {
+        if (N <= 0 || T <= 0) {
+            throw new IllegalArgumentException("N and T should be greater than zero.");
+        }
+        // calculate the mean.
+        openls = new int[T];
+        double mean_sum = 0;
+        for (int i = 0; i < T; i += 1) {
+            Percolation temp = pf.make(N);
+            while (!temp.percolates()) {
+                temp.open((int) StdRandom.uniform(N), (int) StdRandom.uniform(N));
+            }
+            openls[i] = temp.numberOfOpenSites();
+            mean_sum += temp.numberOfOpenSites();
+        }
+        mean = mean_sum / T;
+        // calculate the stddev.
+        double std_sum = 0;
+        for (int i = 0; i < T; i += 1) {
+            std_sum += (openls[i] - mean) * (openls[i] - mean);
+        }
+        stddev = std_sum / (T - 1);
+        confidencelow = mean - 1.96 * Math.sqrt(stddev) / Math.sqrt(T);
+        confidencehigh = mean + 1.96 * Math.sqrt(stddev) / Math.sqrt(T);
+    }
+
+    public double mean() {
+        return mean;
+    }
+
+    public double stddev() {
+        return stddev;
+    }
+
+    public double confidenceLow() {
+        return confidencelow;
+    }
+
+    public double confidenceHigh() {
+        return confidencehigh;
+    }
 
 }
+
